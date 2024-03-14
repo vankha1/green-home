3572877ac69da10ed8e0b7c702d2c5e71b2e543e
perckgg <khangtuyhoa123@gmail.com>
2024-03-14 09:08:38 +0700

    update Backend

---
+++ backend/config/adafruit.ts
@@ -0,0 +1,3 @@
+export const ADAFRUIT_IO_USERNAME: String = 'roopin216'
+export const ADAFRUIT_IO_KEY: String = 'aio_BLqs54VS9RXR3lyiUFuTJlkUmk1y'
+export const ADAFRUIT_IO_FEEDS: String = ADAFRUIT_IO_USERNAME + '/feeds/'
