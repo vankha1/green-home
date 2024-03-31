b6188ef50e1dac8b542f59fd3678c157d56937bc
vankhanh02 <khanhvvan202@gmail.com>
2024-04-01 00:26:34 +0700

    adjust enviroment card UI

--- mobile/src/components/EnviromentCard/EnviromentCard.tsx
+++ mobile/src/components/EnviromentCard/EnviromentCard.tsx
@@ -11,6 +11,9 @@ interface EnviromentCardProps {
   data: number;
   bgColor: string;
   iconName: React.ComponentProps<typeof MaterialCommunityIcons>["name"];
+  measure: string;
+  lowerLimit: number;
+  upperLimit: number;
 }
 
 const EnviromentCard = ({
@@ -19,23 +22,26 @@ const EnviromentCard = ({
   nameDevice,
   bgColor,
   iconName,
+  measure,
+  lowerLimit,
+  upperLimit
 }: EnviromentCardProps) => {
   const [state, setState] = useState<boolean>(false);
 
   return (
     <View style={[styles.container, { backgroundColor: bgColor }]}>
-      <View style={styles.info}>
+      <View style={styles.infoleft}>
         <Text style={{ color: "#fff", fontSize: 16 }}>{nameSensor}</Text>
         <Text style={{ color: "#fff", fontSize: 48 }}>{data}</Text>
-        <Text style={{ color: "#fff", fontSize: 14 }}>{"\u00B0"}C</Text>
+        <Text style={{ color: "#fff", fontSize: 14 }}>{measure}</Text>
       </View>
-      <View style={{ height: 100, width: 2, backgroundColor: "#FFFFFFB3" }} />
-      <View style={styles.info}>
+      <View style={{ height: 100, width: 2, backgroundColor: "#FFFFFFB3"}} />
+      <View style={styles.inforight}>
         <Text style={{ color: "#fff", fontSize: 16 }}>{nameDevice}</Text>
         <View style={styles.line}>
           <MaterialCommunityIcons name="alert" size={16} color="white" />
           <Text style={{ color: "#fff", marginHorizontal: 5 }}>
-            15 - 30 {"\u00B0"}C
+            {lowerLimit} - {upperLimit} {measure}
           </Text>
         </View>
         <TouchableOpacity
